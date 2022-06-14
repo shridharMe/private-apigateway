c3b9fabb4081cd9bd76da1953bfab1137f43dcbc
Shridhar <36041359+shridharMe@users.noreply.github.com>
2022-06-14 21:29:58 +0100

    Update main.tf

--- modules/vpce/main.tf
+++ modules/vpce/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_endpoint" "apigw" {
   vpc_id              = var.vpc_id
-  service_name        = var.service_name      #"com.amazonaws.us-east-2.execute-api"
-  vpc_endpoint_type   = var.vpc_endpoint_type #"Interface"
+  service_name        = var.service_name
+  vpc_endpoint_type   = var.vpc_endpoint_type
   security_group_ids  = [var.security_group_ids]
   subnet_ids          = var.subnets
   private_dns_enabled = true
