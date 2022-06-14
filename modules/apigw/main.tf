a63658b3adf85c6cd9044f951ec056dcbed15fc2
Shridhar Patil <saspatil@amazon.co.uk>
2022-06-14 21:33:10 +0100

    code formated

--- modules/apigw/main.tf
+++ modules/apigw/main.tf
@@ -96,5 +96,5 @@ resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = each.value.lambda_arn
   principal     = "apigateway.amazonaws.com"
-  source_arn = join("", [aws_api_gateway_rest_api.example.execution_arn, "/*/*/*"])
+  source_arn    = join("", [aws_api_gateway_rest_api.example.execution_arn, "/*/*/*"])
 }
